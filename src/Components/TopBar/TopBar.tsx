65b38b800be3cd5b5f2d801f9002e6eb7f6b4a06
temujins <mdsifatulislam.rabbi@gmail.com>
2021-08-09 15:15:57 +0600

    topbar style completed

--- src/Components/TopBar/TopBar.tsx
+++ src/Components/TopBar/TopBar.tsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { meImg } from "../../Images";
 
 const TopBar: React.FC = () => {
   return (
     <div className="top">
-      <div className="top-left">left</div>
-      <div className="top-center">center</div>
-      <div className="top-right">right</div>
+      <div className="top-icon top-left">
+        <i className="top-icon fab fa-facebook-square"></i>
+        <i className="top-icon fab fa-twitter-square"></i>
+        <i className="top-icon fab fa-instagram-square"></i>
+      </div>
+      <div className="top-center">
+        <ul className="top-list">
+          <li className="top-list-item">Home</li>
+          <li className="top-list-item">About</li>
+          <li className="top-list-item">Contact</li>
+          <li className="top-list-item">Write</li>
+          <li className="top-list-item">Logout</li>
+        </ul>
+      </div>
+      <div className="top-right">
+        <img className="top-img" src={meImg} alt="Me" />
+        <i className="top-icon search fas fa-search"></i>
+      </div>
     </div>
   );
 };
---
+++ src/Images/index.ts
@@ -0,0 +1,1 @@
+export { default as meImg } from "./me_image_1mb.png";
