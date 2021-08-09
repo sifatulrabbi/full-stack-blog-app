5afa8be024b9ea34337182a7cac60653875977eb
temujins <mdsifatulislam.rabbi@gmail.com>
2021-08-09 14:16:22 +0600

    adding topbar

---
+++ src/Components/TopBar/TopBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TopBar = () => {
+  return (
+    <div className="top">
+      <div className="top-left">left</div>
+      <div className="top-center">center</div>
+      <div className="top-right">right</div>
+    </div>
+  );
+};
+
+export default TopBar;
