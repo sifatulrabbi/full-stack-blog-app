b7778b8c5c3bf5141ad3a2592f72b7109688d647
temujins <mdsifatulislam.rabbi@gmail.com>
2021-08-10 12:47:11 +0600

    single page post

---
+++ src/Components/SinglePost/SinglePost.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SinglePost: React.FC = () => {
+  return (
+    <div>
+      <p>Single page post</p>
+    </div>
+  );
+};
+
+export default SinglePost;
--- src/Components/index.ts
+++ src/Components/index.ts
@@ -3,3 +3,4 @@ export { default as Header } from "./Header/Header";
 export { default as Sidebar } from "./Sidebar/Sidebar";
 export { default as Posts } from "./Posts/Posts";
 export { default as Post } from "./Post/Post";
+export { default as SinglePost } from "./SinglePost/SinglePost";
