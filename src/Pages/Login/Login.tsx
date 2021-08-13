2d41e0ee47da20fb913407f1d87dbb55915fbaa3
temujins <mdsifatulislam.rabbi@gmail.com>
2021-08-13 17:56:00 +0600

    login page styled

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { img1 as img } from '../../Images';
 
 const Login: React.FC = () => {
   return (
-    <div>
-      <p>Login page</p>
+    <div className="login">
+      <div className="loginWrapper">
+        <span className="loginTitle">Login</span>
+        <form className="loginForm">
+          <label>Email</label>
+          <input type="email" placeholder="Enter your email" />
+          <label>Password</label>
+          <input type="password" placeholder="Enter your password" />
+          <button className="loginBtn">Login</button>
+          <button className="loginBtn reg">Register</button>
+        </form>
+      </div>
     </div>
   );
 };
