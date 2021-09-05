0990462f0412958f8e85fb0039857163c20c0a97
temujins <mdsifatulislam.rabbi@gmail.com>
2021-09-05 08:35:29 +0600

    connect to backend

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const multer = require('multer');
+const cors = require('cors');
 
 const authRoute = require('./routes/auth');
 const usersRoute = require('./routes/users');
@@ -11,6 +12,7 @@ const categoryRoute = require('./routes/categories');
 
 const port = 5000;
 
+app.use(cors());
 dotenv.config();
 app.use(express.json());
 
--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -1,7 +1,18 @@
-import React from "react";
-import { Header, Sidebar, Posts } from "../../Components";
+import React from 'react';
+import axios from 'axios';
+import { Header, Sidebar, Posts } from '../../Components';
 
 const Home: React.FC = () => {
+  const [posts, setPosts] = React.useState<any[]>([]);
+
+  React.useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get('http://localhost:5000/api/posts/');
+      console.log(res.data);
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <>
       <Header />
